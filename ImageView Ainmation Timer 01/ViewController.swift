315941245bc9c9962186e7bbfca36a7c8acf7c3f
D7703_07 <d7703_07@lulzsec.local>
2019-03-27 16:30:48 +0900

    이미지 타이머

--- ImageView Ainmation Timer 01/ViewController.swift
+++ ImageView Ainmation Timer 01/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienTmageViewer: UIImageView!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 1
+    var isAnimation = false
+    var myTimer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienTmageViewer.image = UIImage(named: "Image1")
+       // countLabel.text = String(count)
     }
 
-
+    @IBAction func playBtnPressed(_ sender: Any) {
+        print("play pressed")
+       //Timer 호출
+        myTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        print("pause pressed")
+        myTimer.invalidate()
+    }
+    
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        print("stop pressed")
+        
+        myTimer.invalidate()
+        count = 0
+        countLabel.text = String(count)
+    }
+    
+    @objc func doAnimation() {
+        
+        //count+=1
+        if count == 5{
+            count = 1
+        }else {
+            count+=1
+        }
+        
+        alienTmageViewer.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
+    }
 }
+
 
